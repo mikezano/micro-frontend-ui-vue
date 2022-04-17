f56602c6b8e664d7792221a9799c6cd687a862a3
mikezano <mikezano@gmail.com>
2022-04-17 16:47:31 -0700

    webpack

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -18,6 +18,9 @@ const prodConfig = {
       exposes: {
         './VueApp': './src/bootstrap',
       },
+      remotes: {
+        styles: `styles@${domain}/Styles/latest/remoteEntry.js`,
+      },
       shared: packageJson.dependencies,
     }),
   ],
